2e501baea7b28f3351605847517a486214857269
Bryan Campuzano <bryan.campuzano82@gmail.com>
2016-09-28 11:14:55 -0700

    sli

--- chatbot.js
+++ chatbot.js
@@ -7,9 +7,12 @@ function askQuestion() {
 }
 
 var answer = {
-	"hi" : "hey",
+	"hi" : "hey"<br>,
 	"hey":"OMG I have a Boyfriend",
-	"how old are you?": "21 but I am taken",
-	"what is your name": "EvieBot",
-	"what time is it?": "I got no watch"
+	"how old are you?": "21 but I am taken<br>",
+	"what is your name?": "EvieBot<br>",
+	"what time is it?": "I got no watch<br>",
+	"what day is it?": "I got no calendar<br>",
+	"what is your name?": "EvieBot<br>"
+
 }
